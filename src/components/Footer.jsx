3a0863f0552f0b67a5d7a56d14ae8481f9013b9c
Charly@76 <andigutroag@hotmail.com>
2025-10-11 17:34:28 -0300

    nuevos cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-// src/components/Footer.jsx
 import React from 'react';
 
 const Footer = () => {
   return (
     <footer style={{ backgroundColor: '#eee', padding: '1rem 2rem', marginTop: '2rem', textAlign: 'center' }}>
       <p>© {new Date().getFullYear()} Mi Tienda Online. Todos los derechos reservados.</p>
+      <p>Comisión 25235 React -- Talento Tech.</p>
+       <p>Buenos Aires - Argentina.</p>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-// src/components/Header.jsx
 import React from 'react';
 import Navbar from './Navbar';
 
 const Header = ({ cartItems }) => {
   return (
     <header style={{ backgroundColor: '#eee', padding: '1rem 2rem' }}>
-      <h1>Mi Tienda Online</h1>
+      <h1>Mi Tienda Online Comisión 25235</h1>
       <Navbar cartItems={cartItems} />
     </header>
   );
